60403f6cba60e7e5050c1a46b63deba4630c61e4
sandrabachinger-raithofer <sandra.bachinger-raithofer@stud.fhcampus-wien.ac.at>
2021-06-26 12:42:19 +0200

    neue datenbank

--- Database/config_onlineDatabase.js
+++ Database/config_onlineDatabase.js
@@ -1,10 +1,14 @@
 
-const DB_USER = "sql11420152";
+/*const DB_USER = "sql11420152";
 const DB_PASS = "PvfSsR7xYC";
 const DB_HOST = "sql11.freemysqlhosting.net";
-const DB_NAME = "sql11420152";
+const DB_NAME = "sql11420152";*/
+
+const DB_USER = "sql11421508";
+const DB_PASS = "PkVbtWZzXx";
+const DB_HOST = "sql11.freemysqlhosting.net";
+const DB_NAME = "sql11421508"
 
 module.exports = {DB_USER, DB_PASS, DB_HOST, DB_NAME};
-
 
 
